bbeb8c02ae8f17a00272a61491e166cf9dea608c
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-08-20 15:29:28 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT') ?: '3306';
-$db   = getenv('DB_DATABASE');
-$user = getenv('DB_USERNAME');
-$pass = getenv('DB_PASSWORD');
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT') ?: '3306';
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
 
 try {
 
@@ -15,17 +15,16 @@ try {
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false
         ]
     );
 
 } catch (PDOException $e) {
 
-    http_response_code(500);
+    header('Content-Type: application/json');
 
     echo json_encode([
-        "success" => false,
-        "message" => "Database connection failed."
+        'success' => false,
+        'message' => 'Database connection failed.'
     ]);
 
     exit;
