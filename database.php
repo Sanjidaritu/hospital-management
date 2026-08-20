af7380f04a00665dcea607fcbf17971a982551df
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-08-20 15:31:15 -0400

    Update database.php

--- database.php
+++ database.php
@@ -14,7 +14,7 @@ try {
         $pass,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]
     );
 
