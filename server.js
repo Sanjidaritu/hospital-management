89b1416219c815e3357b8ea33e9fb46809b440b2
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-08-24 15:54:05 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));
+
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    waitForConnections: true,
+    connectionLimit: 10
+});
